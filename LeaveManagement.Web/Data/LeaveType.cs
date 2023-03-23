6109a73bb72021f6acbcdc42145deeb175078b51
Tomislav Robic <tomislav.robic@supersport.hr>
2023-03-23 14:30:57 +0100

    Added Database Tables and Migrations. End Implement Database Section

---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveType: BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public int DefaultDays { get; set; }
+    }
+}
